77e439f4593b4917723087354f3da3818b4f6510
AkeKung <46592342+AkeKung@users.noreply.github.com>
2020-04-12 22:40:51 +0700

    first

---
+++ web-react/build/precache-manifest.859a07836bde2b42f8c7e39fd5fccd2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a287b8913d4ac3a3e6e4775b39407d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72d29f4958249e913a8a",
+    "url": "/static/css/2.b9b2dd63.chunk.css"
+  },
+  {
+    "revision": "60c5a1286c4bd5c23abc",
+    "url": "/static/css/main.4a6fb519.chunk.css"
+  },
+  {
+    "revision": "72d29f4958249e913a8a",
+    "url": "/static/js/2.e446ca26.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e446ca26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c5a1286c4bd5c23abc",
+    "url": "/static/js/main.86f25284.chunk.js"
+  },
+  {
+    "revision": "0d3bbb528df5eb49ddfc",
+    "url": "/static/js/runtime-main.caa9dce7.js"
+  }
+]);
